f11ddfb8a2c062edf024f645902a38122d7a7985
Prem Rathod <prem.rathod.2715@gmail.com>
2026-09-17 17:18:43 +0530

    Understanding LifeCycle of Maven

---
+++ MavenDemo/src/main/java/com/example/Main.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import com.example.controller.DemoController;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        DemoController demoController = new DemoController();
+        System.out.println(demoController.hello());
+
+        }
+    }
+
---
+++ MavenDemo/src/main/java/com/example/controller/DemoController.java
@@ -0,0 +1,7 @@
+package com.example.controller;
+
+public class DemoController {
+    public String hello() {
+        return "hello";
+    }
+}
