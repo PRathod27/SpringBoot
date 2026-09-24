93486013ca31506b049719622409f29d4d92eedd
Prem Rathod <prem.rathod.2715@gmail.com>
2026-09-24 23:54:47 +0530

    When to use Autowired

--- SpringCoreDemo/src/main/java/com/example/OrderService.java
+++ SpringCoreDemo/src/main/java/com/example/OrderService.java
@@ -6,18 +6,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderService {
 
-    // Field Injection
-    @Autowired
+    // Field Injection is not recommended
+//    @Autowired
     private PaymentService paymentService;
 
-//    @Autowired
-//    public OrderService(PaymentService paymentService){
-//        this.paymentService=paymentService;
-//    }
-
-    public void setPaymentService(PaymentService paymentService) {
-        this.paymentService = paymentService;
+    // Constructor injection is most recommended injection type and I can run this code  without using autowrired when there is only one constructor.
+    public OrderService(PaymentService paymentService){
+        this.paymentService=paymentService;
     }
+
+//    public void setPaymentService(PaymentService paymentService) {
+//        this.paymentService = paymentService;
+//    }
 
     public void placeOrder() {
         paymentService.pay();
